4e6567114b538bd929aa5db6ee60bd83ab6cce27
farestz <farestz@gmail.com>
2016-06-13 13:06:45 +0300

    version bump

--- myshows/api.py
+++ myshows/api.py
@@ -1,7 +1,7 @@
 from myshows.urls import *
 from myshows.exceptions import *
 
-VERSION = '0.0.3.3'
+VERSION = '0.0.3.5'
 SHOW_STATUS = ['watching', 'cancelled', 'later', 'remove']
 
 class session(object):
